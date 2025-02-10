04bb80deae587335cbc6b615c8b2957dc8aba3d3
gabefdl <gabe.hfdl@gmail.com>
2025-02-10 16:13:06 -0400

    desktop

--- menu.js
+++ menu.js
@@ -2,3 +2,19 @@ window.addEventListener("scroll", function(){
     var header = this.document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+
+const myObserver = new IntersectionObserver((entra) => {
+    entra.forEach( (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('mostre')
+        } else {
+            entry.target.classList.remove('mostre')
+        }
+    })
+ })
+
+    const fundcadeira = document.querySelector('.fundocadeira')
+
+
+  fundcadeira.forEach( (element) => myObserver.observe(element)) 
